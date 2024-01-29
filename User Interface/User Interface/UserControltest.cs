b828e388f09a69c42b3076eb7c2d98c2c7da372a
bilel0509 <121586206+easilybuild@users.noreply.github.com>
2024-01-29 21:42:59 +0100

    test

---
+++ User Interface/User Interface/UserControltest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Interface
+{
+    public partial class UserControltest : UserControl
+    {
+        public UserControltest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+        }
+    }
+}
