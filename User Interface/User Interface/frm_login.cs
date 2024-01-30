7018b1de1cf25a80faa3bf634161d0b0a6f11740
bilel0509 <121586206+easilybuild@users.noreply.github.com>
2024-01-30 22:39:49 +0100

    removed login frame

--- User Interface/User Interface/frm_login.cs
+++ User Interface/User Interface/frm_login.cs
@@ -1,4 +1,6 @@
 ﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using Pharma_Libarary.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,5 +22,88 @@ namespace User_Interface
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue900, Primary.Blue700, Primary.LightBlue700, Accent.LightBlue700, TextShade.WHITE);
         }
+
+
+
+        private void BTN_Connecter_Click(object sender, EventArgs e)
+        {
+            if (authLogin(tb_userName.Text, tb_password.Text))
+            {
+                enter_theapp();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("invalid creditianls");
+                reset_textboxes(tb_userName, tb_password);
+            }
+        }
+
+
+        /// <summary>
+        /// does the login shit
+        /// </summary>
+        /// <param name="username">username input</param>
+        /// <param name="password">pass input </param>
+        /// <returns> return a bool , if it finds matching cridintials it returns true , else => faalse</returns>
+
+        private bool authLogin(string username, string password)
+        {
+
+
+
+            /// takes
+            using (dbcontext dbc = new dbcontext())
+            {
+
+                var logincrid = dbc.Users.FirstOrDefault(e => e.userName == username && e.password == password);
+
+
+                return logincrid != null;
+
+
+            }
+
+
+
+
+        }
+
+        /// <summary>
+        /// creates a frame
+        /// </summary>
+        private void enter_theapp()
+        {
+
+            MainPage mp = new MainPage();
+
+            mp.ShowDialog();
+
+            this.Close();
+
+
+
+
+        }
+        /// <summary>
+        /// rests the textbots for a new try at loging in
+        /// </summary>
+        /// <param name="textBoxes"></param>
+        private void reset_textboxes(params MaterialTextBox[] textBoxes)
+        {
+
+            foreach (MaterialTextBox tb in textBoxes)
+            {
+
+
+                tb.Text = "";
+
+
+            }
+
+
+        }
+
+     
     }
 }
